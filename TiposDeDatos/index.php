7ecb5a67cc36a85ce9c8d270414725ebbc2358dc
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 23:09:09 -0600

    Add files via upload

---
+++ TiposDeDatos/index.php
@@ -0,0 +1,17 @@
+<?php
+    # "" doble comilla para cadena de strings sin las comillas se considera como número
+    $entero = 123;
+    $decimal = 2.356;
+    $nombre = "Mi nombre es 'D3L10N' y tengo \"21\" de edad y tengo \$5 pesos xd";
+
+    echo "Número entero: $entero";
+    echo "<br>";
+    echo "Número decimal: $decimal";
+    echo "<br>";
+    echo $nombre;
+
+    #manejo de booleanas
+    echo "<br>";
+    $bool = true;
+    echo  $bool;
+?>
